6b510a49eb20724c9177611130387b79c79566bb
Saleh Hassan <saleh.rasta@gmail.com>
2020-11-03 20:30:54 +0100

    feat: edit font size, font color and font family

--- app.js
+++ app.js
@@ -19,15 +19,62 @@ function updateGreetingTextValue(e) {
 }
 
 function changeBgColor() {
-    let selectedBgColor = document.getElementById('chooseCardBgColor');
-    let card = document.getElementById('card');
+    const selectedBgColor = document.getElementById('chooseCardBgColor');
+    const card = document.getElementById('card');
     let choosenBgColor=  selectedBgColor.value;
     card.style.backgroundColor =getColorNameFromValue(choosenBgColor)
 
   }
 
-  function getColorNameFromValue(value){
-      
+  function changeTxtColor() {
+    const selectedBgColor = document.getElementById('chooseCardTextColor');
+    const card = document.getElementById('card');
+    let choosenBgColor=  selectedBgColor.value;
+    card.style.color = getColorNameFromValue(choosenBgColor)
+  }
+
+  function changeTxtSize() {debugger;
+    const selectedBgColor = document.getElementById('chooseCardTextSize');
+    const card = document.getElementById('card');
+    let choosenBgColor=  selectedBgColor.value;
+    card.style.fontSize = getSizeFromValue(choosenBgColor)
+  }
+
+  function chooseCardTextFamily() {
+    const selectedBgColor = document.getElementById('chooseCardTextFamily');
+    const card = document.getElementById('card');
+    let choosenBgColor=  selectedBgColor.value;
+    card.style.fontFamily= getFontFamilyFromValue(choosenBgColor)
+  }
+
+  function getSizeFromValue(value){    
+    if(value==1){
+        return "15px";
+    }
+    else if(value==2){
+        return "20px"
+    }
+    else if(value==3){
+        return "25px"
+    }
+  }
+
+  function getFontFamilyFromValue(value){    
+    if(value==1){
+        return "Times New Roman";
+    }
+    else if(value==2){
+        return "Arial"
+    }
+    else if(value==3){
+        return "Courier New"
+    }
+    else if(value==4){
+        return "Lucida Console"
+    }
+  }
+
+  function getColorNameFromValue(value){    
     if(value==1){
         return "blue";
     }
@@ -39,16 +86,6 @@ function changeBgColor() {
     }
     else if(value==4){
         return "white"
-    }
-
-    function changeTextColor() {
-        let selectedBgColor = document.getElementById('chooseCardTextColor');
-        let card = document.getElementById('card');
-        let choosenBgColor=  selectedBgColor.value;
-        card.style.color =getColorNameFromValue(choosenBgColor)
-    
-      }
-    
-
+    }  
   }
   
